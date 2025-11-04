51555a379ca7c91d52a2f4aa65ad425446fa81d7
Francisco <fvpnuncio@gmail.com>
2025-11-04 17:54:36 +0000

    feat($draw): working and added flag "options:true/false".

--- recordm/customUI/js/cob/_draw.js
+++ recordm/customUI/js/cob/_draw.js
@@ -6,6 +6,9 @@ function handleInstanceCustomizations() {
    const readOnlyMatcher = /[$]readonly/;
    const fileMatcher = /[$]file/;
    const canvasMatcher = /[$]draw/;
+   const widthRegex = /\$draw\(\[.*width:(\d+).*\]\)/;
+   const heightRegex = /\$draw\(\[.*height:(\d+).*\]\)/;
+   const optionsRegex = /\$draw\(\[.*options:(true|false).*\]\)/;
 
    window.defaultView = null
    const canvasToUploadMap = new Map();
@@ -43,6 +46,53 @@ function handleInstanceCustomizations() {
       return request;
    }
 
+   function fetchAndDrawFile(instance, fp, fileName, canvasId) { //show the draw made on the instance
+      const url = `recordm/instances/${instance.data.attachmentPath}/files/${fp.field.fieldDefinition.id}/${fileName}`;
+
+      fetch(url)
+         .then(response => {
+            if (!response.ok) throw new Error(`Failed to fetch image: ${response.statusText}`);
+            return response.blob();
+         })
+         .then(blob => {
+            // Convert the blob to a Data URL instead of a blob: URL
+            const reader = new FileReader();
+            reader.onload = function () {
+            const dataUrl = reader.result;
+            drawImageToCanvas(dataUrl, canvasId);
+            };
+            reader.readAsDataURL(blob);
+         })
+         .catch(err => console.error("Error fetching or drawing image:", err));
+   }
+
+      function drawImageToCanvas(imageUrl, canvasId) {
+      const board = document.getElementById(canvasId);
+      if (!board) {
+         console.error("Drawing board not found for id:", canvasId);
+         return;
+      }
+
+      const canvas = board.querySelector("canvas.drawing-board-canvas");
+      if (!canvas) {
+         console.error("Canvas element not found in drawing board");
+         return;
+      }
+
+      const ctx = canvas.getContext("2d");
+      const img = new Image();
+
+      img.onload = function () {
+         ctx.clearRect(0, 0, canvas.width, canvas.height);
+         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      };
+
+      // Use data URL, not blob:
+      img.src = imageUrl;
+   }
+
+
+
    cob.custom.customize.push(function (core, utils, ui) {
       core.customizeAllInstances((instance, presenter) => {
          if (presenter.isGroupEdit()) return;
@@ -50,19 +100,20 @@ function handleInstanceCustomizations() {
          const canvasFPs = presenter.findFieldPs((fp) => canvasMatcher.exec( fp.field.fieldDefinition.description ) 
          && fileMatcher.exec( fp.field.fieldDefinition.description ));
 
-         const widthRegex = /\$draw\(\[.*width:(\d+).*\]\)/;
-         const heightRegex = /\$draw\(\[.*height:(\d+).*\]\)/;
-
          canvasFPs.forEach((fp) => {
-            if(!instance.isNew() && fp.field.fieldDefinition.description.match(readOnlyMatcher)){
-               return
-            }
             let id = Date.now().toString();
+            if(fp.getValue()){
+               fetchAndDrawFile(instance, fp, fp.getValue(), id)
+            }
+
             const canvasDivParent = $(
                `<div class='dollarDrawingBoard' id=${id}></div>`
             )[0];
+
+            //flag values
             let width = getRegexValue(fp.field.fieldDefinition.description,widthRegex)
             let height = getRegexValue(fp.field.fieldDefinition.description,heightRegex)
+            let options = getBooleanFlag(fp.field.fieldDefinition.description,optionsRegex)
 
             if(width){
                canvasDivParent.style.width = width
@@ -70,85 +121,91 @@ function handleInstanceCustomizations() {
             if(height){
                canvasDivParent.style.height = height
             }
+
             let fieldPresenter = fp.content()[0];
 
             let controlDIV = fieldPresenter.querySelector(".controls")
             controlDIV.style.display="none"
-                       
+
             controlDIV.parentElement.appendChild(canvasDivParent)
 
-            let myBoard = new DrawingBoard.Board(id,{droppable:true,webStorage:false,
+            let myBoard = new DrawingBoard.Board(id,{
+               droppable:true,webStorage:false,
                controls:[{ Navigation: { forward: false, back: false }}]});
-            myBoard.addControl('Download');
-            let imgLink;
-            if(!instance.isNew() 
-            && (imgLink = fp.field.fieldDefinition.description.match(imageMatcher)
-                  ? $(fieldPresenter).find(".link-container a")[0] && $(fieldPresenter).find(".link-container a")[0].href
-                  : null)
-               )
-            {
-               let img = new Image();
-               img.onload = function() {
-                  scaleAndCenterImage(img,myBoard.canvas.getContext("2d"),1)
-               };
-               img.src = imgLink;
-            }
-            //CREATE AN UPLOAD BUTTON TO INSERT AN IMAGE TO CANVAS   
-            const addImageButton = $(`<div class="drawing-board-control"> 
-               <input type="file" id="file_${id}" name="name_${id}">
-               <button class="btn btn-small "> <i class="icon-upload"></i> </button>
-            </div>`)[0]
-            canvasDivParent.children[0].appendChild(addImageButton)
-            addImageButton.children[1].onclick=(evt)=>{
-               addImageButton.children[1].blur()
-               addImageButton.children[0].click()
-            }
 
-            addImageButton.children[0].onchange=(evt)=>{
-               
-               let img = new Image();
-               let file =  evt.target.files[0];
-               if(file && file.type.match('image.*')) {
-                  var reader = new FileReader();
-                  // Read in the image file as a data URL.
-                  reader.readAsDataURL(file);
-                  reader.onload = function(evt){
-                     if( evt.target.readyState == FileReader.DONE) {
-                        img.src = evt.target.result;
-                        img.onload = ()=>{
-                           scaleAndCenterImage(img,  myBoard.canvas.getContext("2d"),0.95)
-                           stoppedDrawingHandler()
-                        }
-                     }
-                  }    
-               } else {
-                  console.debug("File loaded is not an image!")
-               }
-            }
+            let drawing = false;
 
-            myBoard.ev.bind('board:reset', ()=>{
-               if(imgLink){
-                  let obj = canvasToUploadMap.get(fp.field.fieldDefinition.id)
-                  if(!obj){
-                     obj = {canvas:myBoard.canvas,instance:instance,fp:fp,op:"DELETE"}
-                     canvasToUploadMap.set(fp.field.fieldDefinition.id,obj)
-                  }
-                  obj.op = "DELETE"
-               }else{
-                  canvasToUploadMap.delete(fp.field.fieldDefinition.id)
-               }
+            myBoard.ev.bind('board:startDrawing', () => {
+               drawing = true;
+               // Add global listener for mouse release
+               document.addEventListener('mouseup', globalStopHandler);  //in case the user leaves the area without releasing
             });
-            
-            myBoard.ev.bind('board:stopDrawing', ()=>{
+
+            myBoard.ev.bind('board:stopDrawing', () => {
+               //stoped drawing inside of the area 
                stoppedDrawingHandler()
             });
 
-            function stoppedDrawingHandler() {     
-               if(!canvasToUploadMap.has(fp.field.fieldDefinition.id)){
-                  canvasToUploadMap.set(fp.field.fieldDefinition.id,{canvas:myBoard.canvas,instance:instance,fp:fp,op:"POST"})
+            function globalStopHandler() {
+               if (drawing) {
+                  //stoped drawing outside of the area 
+                  stoppedDrawingHandler();
+                  cleanupGlobalHandler();
                }
             }
-            
+
+            function cleanupGlobalHandler() {
+               drawing = false;
+               document.removeEventListener('mouseup', globalStopHandler);
+            }
+
+            function stoppedDrawingHandler() { //add the value to the field 
+               canvasToUploadMap.set(fp.field.fieldDefinition.id, { canvas: myBoard.canvas, instance: instance, fp: fp, op: "POST" })
+            }
+
+            //erase button (always available)
+            myBoard.ev.bind('board:reset', () => {
+               let obj = { canvas: myBoard.canvas, instance: instance, fp: fp, op: "DELETE" }
+               canvasToUploadMap.set(fp.field.fieldDefinition.id, obj)
+            });
+
+            if (options) {//flag options:true so show options
+
+               myBoard.addControl('Download');
+
+               //CREATE AN UPLOAD BUTTON TO INSERT AN IMAGE TO CANVAS   
+               const addImageButton = $(`<div class="drawing-board-control"> 
+                  <input type="file" id="file_${id}" name="name_${id}">
+                  <button class="btn btn-small "> <i class="icon-upload"></i> </button>
+               </div>`)[0]
+               canvasDivParent.children[0].appendChild(addImageButton)
+               addImageButton.children[1].onclick=(evt)=>{
+                  addImageButton.children[1].blur()
+                  addImageButton.children[0].click()
+               }
+
+               addImageButton.children[0].onchange=(evt)=>{
+                  
+                  let img = new Image();
+                  let file =  evt.target.files[0];
+                  if(file) {
+                     var reader = new FileReader();
+                     // Read in the image file as a data URL.
+                     reader.readAsDataURL(file);
+                     reader.onload = function(evt){
+                        if( evt.target.readyState == FileReader.DONE) {
+                           img.src = evt.target.result;
+                           img.onload = ()=>{
+                              scaleAndCenterImage(img,  myBoard.canvas.getContext("2d"),0.95)
+                              stoppedDrawingHandler()
+                           }
+                        }
+                     }    
+                  } else {
+                     console.debug("File loaded is not an image!")
+                  }
+               }
+            }
 
             function tbnClickFunc(e) {
                if (canvasToUploadMap.size > 0){
@@ -163,10 +220,8 @@ function handleInstanceCustomizations() {
                   })
                }
             }
-            if(!saveBTN){
-               saveBTN = document.getElementsByClassName("js-save-instance")[0]
-               saveBTN.onclick = tbnClickFunc 
-            }
+            saveBTN = document.getElementsByClassName("js-save-instance")[0]
+            saveBTN.onclick = tbnClickFunc 
          });
       })
    });
@@ -178,6 +233,11 @@ function getRegexValue(input,regex) {
       return `${match[1]}px`;
    }
    return null;
+}
+
+function getBooleanFlag(input, regex) { //for true/false regex
+  const match = input.match(regex);
+  return match ? match[1] === "true" : false;
 }
 
 function scaleAndCenterImage(img, ctx,ratioFactor) {
